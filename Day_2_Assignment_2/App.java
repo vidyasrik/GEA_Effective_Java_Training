42220bfda56a42906be6cbc4b9cf6d0f16c1756c
vidyasrik <49954792+vidyasrik@users.noreply.github.com>
2021-10-27 07:03:20 +0530

    Add files via upload

---
+++ Day_2_Assignment_2/App.java
@@ -0,0 +1,21 @@
+
+public class App {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Atm rajahmundry = new Atm();
+		rajahmundry.ba1.show();
+		rajahmundry.ba1.deposit(200000);
+		rajahmundry.ba1.creditInterest();
+		rajahmundry.ba1.withdraw(500,"pass");
+		rajahmundry.ba1.changePassword("pass","new@pass");
+		rajahmundry.ba1.show();		
+		
+
+		
+		
+		
+		
+	}
+
+}
---
+++ Day_2_Assignment_2/Atm.java
@@ -0,0 +1,7 @@
+
+
+public class Atm {
+	public  BankAccount ba1=new BankAccount(2347895, "Vidya","pass", 50000, 12);
+	public  BankAccount ba2=new BankAccount(5478945, "Sri","happy", 50000, 12);
+	public  BankAccount ba3=new BankAccount(9654712, "Suni","word", 50000, 12);
+}
