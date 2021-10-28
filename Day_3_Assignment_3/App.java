ef781aa9e3acf3829159d3c1cf31776c3490f4cd
vidyasrik <49954792+vidyasrik@users.noreply.github.com>
2021-10-28 08:03:57 +0530

    Add files via upload

---
+++ Day_3_Assignment_3/App.java
@@ -0,0 +1,21 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Bank b=new Bank(777,"pa@ss",1500);
+
+		ATM a=new ATM();
+		
+		a.withdraw(777,"p@ss",1500);
+		a.Deposit(777, 5000);
+		
+		.changePassword("p@ss","Vidya@");
+		
+
+		
+		
+	}
+
+}
